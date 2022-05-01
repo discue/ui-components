ec0bb7a72adf0ff02902ca70a599a2e729736a8c
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-01 21:50:49 +0200

    feat: add dropdown menu components

---
+++ src/components/drop-down-menu-item.vue
@@ -0,0 +1,30 @@
+<template>
+    <li class="hover:bg-gray-300 p-4 font-medium group z-10 cursor-pointer">
+        <div class="flex flex-row items-center" @click.prevent="onClick">
+            <div class="text-gray-500 group-hover:text-gray-900">
+                <slot />
+            </div>
+            <span>{{ label }}</span>
+        </div>
+    </li>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+const props = defineProps({
+    label: {
+        type: String
+    },
+    href: {
+        type: String
+    }
+})
+
+function onClick() {
+    router.push({ path: props.href })
+}
+</script>
---
+++ src/components/drop-down-menu.vue
@@ -0,0 +1,32 @@
+<template>
+    <Transition>
+        <div v-if="isVisible" @mouseenter="onFocus" @mouseleave="onBlur"
+            class="bg-gray-50 text-gray-900 bottom-0 absolute shadow-md font-normal rounded-md w-48 border-2 border-gray-200">
+            <ul class="text-left text-lg">
+                <slot />
+            </ul>
+        </div>
+    </Transition>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps, ref } from 'vue';
+
+const props = defineProps({
+    show: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const hasFocus = ref(false)
+const isVisible = computed(() => hasFocus.value || props.show)
+
+function onFocus() {
+    hasFocus.value = true
+}
+
+function onBlur() {
+    hasFocus.value = false
+}
+</script>
