6f9d08d8473125f760bd581b6071ec83bcf63eaf
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-01 21:50:49 +0200

    feat: add dropdown menu components

---
+++ src/components/drop-down-menu-banner-item.vue
@@ -0,0 +1,9 @@
+<template>
+    <li class="p-3 rounded-t-md border-b border-gray-400 cursor-default" @click.prevent>
+        <div class="flex flex-col">
+            <slot name="default"/>
+        </div>
+    </li>
+</template>
+
+<script setup/>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 // make sure to also update /types/index.d.ts
 
 export { default as BackToTop } from './components/back-to-top.vue';
+export { default as DropDownMenuBannerItem } from './components/drop-down-menu-banner-item.vue';
+export { default as DropDownMenuItem } from './components/drop-down-menu-item.vue';
+export { default as DropDownMenu } from './components/drop-down-menu.vue';
 
