c77063bf6345ed1a04fa8ec49c0c77c0a38cf85b
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-04 17:54:11 +0200

    chore(build): set base url and alias

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 // make sure to also update /types/index.d.ts
 
 export { default as BackToTop } from './components/back-to-top.vue';
+export { default as DynamicComponentDisplay } from './components/docs/dynamic-component-display.vue';
 export { default as DropDownMenuBannerItem } from './components/drop-down-menu-banner-item.vue';
 export { default as DropDownMenuItem } from './components/drop-down-menu-item.vue';
 export { default as DropDownMenu } from './components/drop-down-menu.vue';
+export { default as NavLink } from './components/nav-link.vue';
+export { default as Text } from './components/text.vue';
 
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+
 module.exports = defineConfig({
     transpileDependencies: true,
     configureWebpack: {
         externals: [
             'vue', 'vue-router'
-        ]
+        ],
+        resolve: {
+            symlinks: false,
+            alias: {
+                '@': resolve('./src/')
+            }
+        }
     },
     css: {
         extract: false,
