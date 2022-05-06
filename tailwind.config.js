f190af2435a44c826841c728afb3b42f9ac3808f
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-06 22:10:44 +0200

    chore(docs): add custom color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+const colors = require('tailwindcss/colors')
+
+colors.primary = colors.lime
+colors.secondary = colors.pink
+colors.tertiary = colors.indigo
+
 module.exports = {
   content: [
     "./docs/**/*.{vue,js,ts,jsx,tsx,scss,md}",
     "./docs/.vuepress/theme/components/*.{vue,js,ts,jsx,tsx,scss,md}",
-    "./src/**"
+    "./src/**/*.vue"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        attention: colors.rose,
+        primary: colors.primary,
+        secondary: colors.secondary,
+        tertiary: colors.tertiary,
+      },
+    }
   },
   plugins: [],
   corePlugins: {
