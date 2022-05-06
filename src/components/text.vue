9c4857b2e52fc29a01345e1dcf1c0093de45ac65
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-06 19:06:08 +0200

    feat: add text element

---
+++ src/components/text.vue
@@ -0,0 +1,54 @@
+<template>
+    <span :class="clazz">
+        <slot />
+    </span>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    highlight: {
+        type: Boolean,
+    },
+    light: {
+        type: Boolean,
+        default: false,
+    },
+    small: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const fontWeight = computed(() => {
+    if (props.highlight) {
+        return 'font-medium'
+    } else {
+        return ''
+    }
+})
+
+const fontColor = computed(() => {
+    if (props.light) {
+        return 'text-gray-100'
+    } else if (props.highlight) {
+        return 'text-gray-900'
+    } else {
+        return 'text-gray-600'
+    }
+})
+
+const fontSize = computed(() => {
+    if (props.small) {
+        return 'text-sm'
+    } else {
+        return 'text-md'
+    }
+})
+
+const clazz = computed(() => {
+    return `${fontSize.value} ${fontWeight.value} ${fontColor.value}`
+})
+
+</script>
