c9274a3324e6d843ee119f96784588560314ab6f
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-07 08:28:27 +0200

    feat(drop-down-item): add show prop to control visiblity

--- src/components/drop-down-menu-item.vue
+++ src/components/drop-down-menu-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <li class="hover:bg-gray-300 p-4 font-medium group z-10 cursor-pointer">
+    <li v-if="show" class="hover:bg-gray-300 p-4 font-medium group z-10 cursor-pointer">
         <div class="flex flex-row items-center" @click.prevent="onClick">
             <div class="text-gray-500 group-hover:text-gray-900">
                 <slot />
@@ -15,6 +15,10 @@ import { useRouter } from 'vue-router';
 const router = useRouter()
 
 const props = defineProps({
+    show: {
+        type: Boolean,
+        default: true
+    },
     label: {
         type: String
     },
