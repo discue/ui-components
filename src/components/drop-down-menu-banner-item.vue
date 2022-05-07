fae0cd31f835d38b8d0128e199da692107d9dbcf
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-07 08:54:20 +0200

    feat(drop-down-banner): use show prop to control visibility

--- src/components/drop-down-menu-banner-item.vue
+++ src/components/drop-down-menu-banner-item.vue
@@ -1,9 +1,16 @@
 <template>
-    <li class="p-3 rounded-t-md border-b border-gray-400 cursor-default" @click.prevent>
+    <li v-if="show" class="p-3 rounded-t-md border-b border-gray-400 cursor-default" @click.prevent>
         <div class="flex flex-col">
             <slot name="default"/>
         </div>
     </li>
 </template>
 
-<script setup/>
+<script setup>
+defineProps({
+    show: {
+        type: Boolean,
+        default: true
+    }
+})
+</script>
