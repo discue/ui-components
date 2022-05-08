6ee30c8e219a64c9b8752c9b3d7dfdc1404d3676
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-08 20:05:44 +0200

    feat(back-to-top): make offset configurable

--- src/components/back-to-top.vue
+++ src/components/back-to-top.vue
@@ -15,9 +15,16 @@
 import { debounce } from "ts-debounce";
 import { computed, onMounted, ref } from "vue";
 
+const props = defineProps({
+    offset: {
+        type: Number,
+        default: 300
+    }
+})
+
 const scrollTop = ref(0)
 const show = computed(() => {
-    const show = scrollTop.value > 300
+    const show = scrollTop.value > props.offset
     return show
 })
 const onScroll = debounce(() => {
@@ -42,19 +49,6 @@ onMounted(() => {
     window.addEventListener("scroll", () => onScroll())
 })
 </script>
-
-<template>
-    <Transition name="back-to-top">
-        <div v-if="show" @click="scrollToTop" class="back-to-top">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49.484 28.284">
-                <g transform="translate(-229 -126.358)" fill="currentColor">
-                    <rect width="35" height="5" rx="2" transform="rotate(-45 296.902 -200.874)" />
-                    <rect width="35" height="5" rx="2" transform="rotate(-135 169.502 20.377)" />
-                </g>
-            </svg>
-        </div>
-    </Transition>
-</template>
 
 <style>
 .back-to-top {
