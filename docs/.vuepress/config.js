2e2cd169193fd316ec5400e6a3bce9d0ab4c86b0
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-09 18:09:52 +0200

    chore(docs): add viewport meta tag

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -100,6 +100,8 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', type: "image/png", sizes: "16x16", href: "/icons-fire-all-black/web/favicon.ico" }],
         ['link', { rel: 'icon', type: "image/png", sizes: "32x32", href: "/icons-fire-all-black/web/favicon.ico" }],
-        ['link', { rel: "apple-touch-icon", sizes: "152x152", href: "/icons-fire-all-black/web/apple-touch-icon-152x152.png" }]
+        ['link', { rel: "apple-touch-icon", sizes: "152x152", href: "/icons-fire-all-black/web/apple-touch-icon-152x152.png" }],
+        ['viewport', { content: "width=device-width,initial-scale=1.0" }
+        ]
     ],
 }
