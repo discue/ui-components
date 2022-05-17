b09016c7956f472094f94cb32a8f8918e158c1bf
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-18 00:24:47 +0200

    feat(docs): use sitemap plugin

--- docs/.vuepress/configs/plugins.js
+++ docs/.vuepress/configs/plugins.js
@@ -1,5 +1,6 @@
 const { registerComponentsPlugin } = require('@vuepress/plugin-register-components')
 const { shikiPlugin } = require('@vuepress/plugin-shiki')
+const { sitemapPlugin } = require('vuepress-plugin-sitemap2')
 const { path } = require('@vuepress/utils')
 
 module.exports = [
@@ -7,4 +8,10 @@ module.exports = [
         componentsDir: path.resolve(__dirname, '../examples')
     }),
     shikiPlugin({ theme: 'dark-plus' }),
+    sitemapPlugin({
+        hostname: 'https://ui.discue.io',
+        excludeUrls: [
+            'https://docs.discue.io/api-reference/'
+        ]
+    }),
 ]
