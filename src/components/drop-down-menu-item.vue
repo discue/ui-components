14f33498b14673bee6a988b54704321e36c9cbec
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-06-05 18:19:31 +0200

    feat(drop-down-item): align text center

--- src/components/drop-down-menu-item.vue
+++ src/components/drop-down-menu-item.vue
@@ -1,6 +1,6 @@
 <template>
     <li v-if="show" class="hover:bg-gray-300 p-4 font-medium group z-10 cursor-pointer">
-        <div class="flex flex-row items-center" @click.prevent="onClick">
+        <div class="flex flex-row justify-center" @click.prevent="onClick">
             <div class="text-gray-500 group-hover:text-gray-900">
                 <slot />
             </div>
