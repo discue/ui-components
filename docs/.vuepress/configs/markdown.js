d31e824c70697686dd4e421917df8a44f68ef0da
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-06-05 17:26:51 +0200

    chore(markdown): add src alias for code imports

--- docs/.vuepress/configs/markdown.js
+++ docs/.vuepress/configs/markdown.js
@@ -3,6 +3,8 @@ const { path } = require('@vuepress/utils')
 module.exports = {
     importCode: {
         handleImportPath: (str) =>
-            str.replace(/^@examples/, path.resolve(__dirname, '../examples')),
+            str
+                .replace(/^@examples/, path.resolve(__dirname, '../examples'))
+                .replace(/^@src/, path.resolve(__dirname, '../../../src')),
     }
 }
