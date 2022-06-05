886a6691d52b5a94c64d3f47624926db003b8921
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-06-05 19:00:45 +0200

    fix(drop-down-item): icon and text not centered

--- src/components/drop-down-menu-item.vue
+++ src/components/drop-down-menu-item.vue
@@ -1,7 +1,7 @@
 <template>
     <li v-if="show" class="hover:bg-gray-300 p-4 font-medium group z-10 cursor-pointer">
-        <div class="flex flex-row justify-center" @click.prevent="onClick">
-            <div class="text-gray-500 group-hover:text-gray-900">
+        <div class="flex flex-row items-center" @click.prevent="onClick">
+            <div class="flex items-center text-gray-500 group-hover:text-gray-900">
                 <slot />
             </div>
             <span>{{ label }}</span>
