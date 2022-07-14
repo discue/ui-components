c5da50251e74845f78e297f3fd5545551618aab5
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-07-14 10:36:19 +0200

    chore(ci): explicitly set vue cli service config

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -e
 
 rm -rf dist/
 
-npm run build-lib-public-no-ssr
+VUE_CLI_SERVICE_CONFIG_PATH=vue.config.js npm run build-lib-public-no-ssr
 VUE_CLI_SERVICE_CONFIG_PATH=ssr-safe-vue.config.js npm run build-lib-public-for-ssr
-npm run build-lib-internal-no-ssr
+VUE_CLI_SERVICE_CONFIG_PATH=vue.config.js  npm run build-lib-internal-no-ssr
 VUE_CLI_SERVICE_CONFIG_PATH=ssr-safe-vue.config.js npm run build-lib-internal-for-ssr
-npm run build-types
+VUE_CLI_SERVICE_CONFIG_PATH=vue.config.js npm run build-types
 
 rm -rf dist/demo.html
 rm -rf dist/*/demo.html
