ec476b620c3f24040e9c482927f4fa200767685d
stfsy <s.pfaffel@gmail.com>
2023-01-02 21:49:37 +0100

    feat(drop-down): add animation for state transitions

--- src/components/drop-down-menu.vue
+++ src/components/drop-down-menu.vue
@@ -1,7 +1,7 @@
 <template>
-    <Transition>
+    <Transition name="modal">
         <div v-if="isVisible" @mouseenter="onFocus" @mouseleave="onBlur"
-            class="dsq-drop-down-menu bg-gray-50 text-gray-900 fixed shadow-md font-normal rounded-md w-48 border-2 border-gray-200">
+            class="dsq-drop-down-menu bg-gray-50 text-gray-900 fixed shadow-md font-normal rounded-md w-48 border-2 border-gray-200 duration-200 ease-in-out transition">
             <ul class="text-left text-lg">
                 <slot />
             </ul>
@@ -30,3 +30,28 @@ function onBlur() {
     hasFocus.value = false
 }
 </script>
+
+<style scoped>
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ *
+ * https://vuejs.org/examples/#modal
+ */
+
+ .modal-enter-from {
+  opacity: 0;
+}
+.modal-leave-to {
+  opacity: 0;
+}
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
