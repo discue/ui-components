e452d603153b9a8a16bcb322e2ab57d5d7d8f148
stfsy <s.pfaffel@gmail.com>
2023-01-20 20:40:06 +0100

    fix(drop-down-menu-item): not all clicks are captured

--- src/components/drop-down-menu-item.vue
+++ src/components/drop-down-menu-item.vue
@@ -1,6 +1,6 @@
 <template>
-    <li v-if="show" class="dsq-drop-down-menu-item hover:bg-gray-300 p-4 font-medium group z-10 cursor-pointer">
-        <div class="flex flex-row items-center" @click.prevent="onClick">
+    <li v-if="show" class="dsq-drop-down-menu-item hover:bg-gray-300 p-4 font-medium group z-10 cursor-pointer" @click.prevent="onClick">
+        <div class="flex flex-row items-center">
             <div class="flex items-center text-gray-500 group-hover:text-gray-900">
                 <slot />
             </div>
@@ -28,6 +28,7 @@ const props = defineProps({
 })
 
 function onClick() {
+    console.log('onclick')
     if (props.href) {
         router.push({ path: props.href })
     }
