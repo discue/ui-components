e506034b5df83a5049f45e1bb30cd3c7df48aef1
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-09-01 10:23:52 +0200

    chore(input-radio): use less stroke if not selected

--- src/components/form-input-radio.vue
+++ src/components/form-input-radio.vue
@@ -19,11 +19,11 @@
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <svg v-else-if="disabled" xmlns="http://www.w3.org/2000/svg" class="stroke-2 stroke-current h-6 w-6"
+                    <svg v-else-if="disabled" xmlns="http://www.w3.org/2000/svg" class="stroke-1 stroke-current h-6 w-6"
                         fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8  01M21 12a9 9 0 0 9 9 0 0118 0z" />
                     </svg>
-                    <svg v-else xmlns="http://www.w3.org/2000/svg" class="stroke-2 stroke-current h-6 w-6" fill="none"
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" class="stroke-1 stroke-current h-6 w-6" fill="none"
                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8  01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
