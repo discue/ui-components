69ee8e1cb17b9a03b223a99fd0b3c8a77faa40fb
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-09-01 09:45:22 +0200

    chore: add partly refactored theme

--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,41 @@
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 // add only computed properties here to allow for reactivity
+const DEFAULT_BORDER_WIDTH = ref('border-2')
+
+const TOOL_COLOR = ref('gray')
+const TOOL_COLOR_LIGHT_VARIANT = ref('500')
+const TOOL_COLOR_VARIANT = ref('800')
+
+const PRIMARY_COLOR = ref('primary')
+const PRIMARY_COLOR_LIGHT_VARIANT = ref('500')
+const PRIMARY_COLOR_VARIANT = ref('800')
+
+const ATTENTION_COLOR = ref('attention')
+const ATTENTION_COLOR_VARIANT = ref('800')
+
+const TEXT_COLOR = ref('gray')
+const TEXT_COLOR_VARIANT = ref('900')
+
+const TEXT_SIZE_VARIANT = ref('md')
+const TEXT_SIZE_SMALL_VARIANT = ref('sm')
+const TEXT_SIZE_LARGE_VARIANT = ref('lg')
+
 const theme = {
     formElement: {
         borderSize: {
             'default': ref('border-2')
         },
         borderColor: {
-            'default': ref('border-gray-500'),
-            'active': ref('border-gray-800'),
-            'attention': ref('border-attention-800')
+            'default': computed(() => `border-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`),
+            'active': computed(() => `border-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`),
+            'attention': computed(() => `border-${ATTENTION_COLOR.value}-${ATTENTION_COLOR_VARIANT.value}`)
         },
         borderRingSize: {
             'default': ref('hover:ring-2')
         },
         borderRingColor: {
-            'default': ref('hover:ring-primary-500')
+            'default': computed(() => `hover:ring-${PRIMARY_COLOR.value}-${PRIMARY_COLOR_VARIANT.value}`)
         }
     },
     formElementLabel: {
@@ -23,8 +43,8 @@ const theme = {
             'default': ref('text-md')
         },
         color: {
-            'default': ref('text-gray-800'),
-            'attention': ref('text-attention-800')
+            'default': computed(() => `text-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`),
+            'attention': computed(() => `text-${ATTENTION_COLOR.value}-${ATTENTION_COLOR_VARIANT.value}`)
         },
         background: {
             'default': ref('bg-white')
@@ -38,7 +58,7 @@ const theme = {
             'default': ref('text-xs')
         },
         color: {
-            'default': ref('text-gray-400')
+            'default': computed(() => `text-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`),
         },
         weight: {
             'default': ref('font-medium')
@@ -49,8 +69,8 @@ const theme = {
             'default': ref('text-xs')
         },
         color: {
-            'default': ref('text-attention-800'),
-            'hint': ref('text-gray-800')
+            'default': computed(() => `text-${ATTENTION_COLOR.value}-${ATTENTION_COLOR_VARIANT.value}`),
+            'hint': computed(() => `text-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`)
         },
         weight: {
             'default': ref('font-medium')
@@ -63,13 +83,13 @@ const theme = {
             'large': ref('text-lg'),
         },
         color: {
-            'default': ref('text-gray-900'),
-            'light': ref('text-gray-100'),
-            'highlight': ref('text-gray-900')
+            'default': computed(() => `text-${TEXT_COLOR.value}-${TEXT_COLOR_VARIANT.value}`),
+            'light': computed('text-gray-100'),
+            'highlight': computed(() => `text-${TEXT_COLOR.value}-${TEXT_COLOR_VARIANT.value}`)
         },
         weight: {
-            'default': ref('font-base'),
-            'highlight': ref('font-medium'),
+            'default': computed(() => 'font-base'),
+            'highlight': computed(() => 'font-medium'),
         }
     },
     formElementInput: {
@@ -77,16 +97,16 @@ const theme = {
             default: ref('text-lg')
         },
         caretColor: {
-            default: ref('caret-gray-900')
+            default: computed(() => `caret-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`)
         },
         textColor: {
-            default: ref('text-gray-900')
+            default: computed(() => `text-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`)
         },
         backgroundColor: {
             default: ref('bg-white')
         },
         placeholderColor: {
-            default: ref('placeholder:text-gray-500')
+            default: computed(() => `placeholder:text-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`)
         }
     },
     formElementSelect: {
@@ -97,34 +117,34 @@ const theme = {
             'default': ref('font-medium'),
         },
         textColor: {
-            default: ref('text-gray-900')
+            default: computed(() => `text-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`)
         },
     },
     formElementRadio: {
         textColor: {
-            default: ref('text-gray-900'),
-            disabled: ref('text-gray-500')
+            default: computed(() => `text-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`),
+            disabled: computed(() => `text-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`)
         }
     },
     button: {
         background: {
-            'default': ref('bg-gray-900'),
-            'attention': ref('bg-attention-800'),
-            'secondary': ref('bg-gray-500'),
+            'default': computed(() => `bg-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`),
+            'attention': computed(() => `bg-${ATTENTION_COLOR.value}-${ATTENTION_COLOR_VARIANT.value}`),
+            'secondary': computed(() => `bg-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`),
             'inherit': ref('bg-inherit')
         },
         borderColor: {
             'default': ref(),
-            'secondary': ref('hover:border-gray-500'),
-            'attention': ref('hover:border-attention-800'),
-            'inverted': ref('hover:border-gray-900')
+            'secondary': computed(() => `hover:border-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`),
+            'attention': computed(() => `hover:border-${ATTENTION_COLOR.value}-${ATTENTION_COLOR_VARIANT.value}`),
+            'inverted': computed(() => `hover:border-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`)
         },
         color: {
-            'default': ref('text-gray-900'),
-            'attention': ref('text-attention-800'),
-            'secondary': ref('text-gray-500'),
+            'default': computed(() => `text-${TEXT_COLOR.value}-${TEXT_COLOR_VARIANT.value}`),
+            'attention': computed(() => `text-${ATTENTION_COLOR.value}-${ATTENTION_COLOR_VARIANT.value}`),
+            'secondary': computed(() => `text-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`),
             'light': ref('text-gray-100'),
-            'highlight': ref('text-gray-900'),
+            'highlight': computed(() => `text-${TEXT_COLOR.value}-${TEXT_COLOR_VARIANT.value}`),
             'inherit': ref('text-inherit'),
         },
         size: {
@@ -142,17 +162,17 @@ const theme = {
             'large': ref('text-2xl'),
         },
         color: {
-            'default': ref('text-gray-900'),
-            'attention': ref('text-attention-800'),
-            'secondary': ref('text-gray-500'),
+            'default': ref(`text-${TEXT_COLOR.value}-${TEXT_COLOR_VARIANT.value}`),
+            'attention': ref(`text-${ATTENTION_COLOR.value}-${ATTENTION_COLOR_VARIANT.value}`),
+            'secondary': ref(`text-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`),
             'light': ref('text-gray-100'),
-            'highlight': ref('text-gray-900'),
+            'highlight': ref(`text-${TEXT_COLOR.value}-${TEXT_COLOR_VARIANT.value}`),
             'inherit': ref('text-inherit'),
         },
         border: {
-            'default': ref('hover:border-gray-900'),
-            'attention': ref('hover:border-attention-500'),
-            'secondary': ref('hover:border-gray-500'),
+            'default': ref(`hover:border-${TOOL_COLOR.value}-${TOOL_COLOR_VARIANT.value}`),
+            'attention': ref(`hover:border-${ATTENTION_COLOR.value}-${ATTENTION_COLOR_VARIANT.value}`),
+            'secondary': ref(`hover:border-${TOOL_COLOR.value}-${TOOL_COLOR_LIGHT_VARIANT.value}`),
         },
         weight: {
             'default': ref('font-bold')
