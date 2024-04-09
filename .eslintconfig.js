9b24a243d8fefd2028a28a575a7fc614def74493
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-04-09 08:52:10 +0200

    chore: use new eslint version and config

---
+++ .eslintconfig.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js"
+import pluginVue from 'eslint-plugin-vue'
+import eslintParser from 'vue-eslint-parser'
+
+export default [
+    js.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            parser: eslintParser
+        },
+        rules: {
+            'vue/require-default-prop': 'off',
+            'vue/multi-word-component-names': 'off',
+            'vue/html-indent': 'off',
+            'no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                }
+            ],
+            "quotes": [
+                "error",
+                "single",
+                {
+                    "allowTemplateLiterals": true
+                }
+            ]
+        }
+    }
+]
