c1fc5d563445c6af4808cfc3dbd1312696a3f00c
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-06-02 21:32:01 +0200

    feat: use consistent external link icon

--- src/components/nav-link.vue
+++ src/components/nav-link.vue
@@ -6,25 +6,6 @@
        :target="linkTarget"
        @click="click($event)">
         <slot />
-
-        <span v-if="isExternalLink"
-              ref="externalLink"
-              class="ml-1">
-            <svg class="external-link-icon inline-block relative align-middle -top-0.5"
-                 xmlns="http://www.w3.org/2000/svg"
-                 aria-hidden="true"
-                 focusable="false"
-                 x="0px"
-                 y="0px"
-                 viewBox="0 0 100 100"
-                 width="15"
-                 height="15">
-                <path fill="currentColor"
-                      d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z" />
-                <polygon fill="currentColor"
-                         points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9" />
-            </svg>
-        </span>
     </a>
 </template>
 
@@ -113,12 +94,16 @@ const fontWeight = computed(() => {
 })
 
 const clazz = computed(() => {
-    if (props.invert) {
-        return `p-3 ${textColor.value} ${borderColor.value} ${textSize.value} ${bgColor.value} ${fontWeight.value} hover:shadow-md rounded-lg transform transition-transform hover:-translate-y-1`
-    } else {
-        return `${textSize.value} ${textColor.value} ${borderColor.value} animate ${fontWeight.value} hover:border-b-4 cursor-pointer`
-
+    const clazz = []
+    if (isExternalLink.value) {
+        clazz.push('external-link-icon')
     }
+    if (props.invert) {
+        clazz.push(`p-3 ${textColor.value} ${borderColor.value} ${textSize.value} ${bgColor.value} ${fontWeight.value} hover:shadow-md rounded-lg transform transition-transform hover:-translate-y-1`)
+    } else {
+        clazz.push(`${textSize.value} ${textColor.value} ${borderColor.value} animate ${fontWeight.value} hover:border-b-4 cursor-pointer`)
+    }
+    return clazz.join(' ')
 })
 
 const isRelativeLink = computed(() => {
