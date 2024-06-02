7bc8585ef1be48de465777f6b78f7ee9473b5bcd
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-06-02 21:30:33 +0200

    chore: override page meta to not include footer tag

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# replace duplicate footer tag
+sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+
 npx vite build --config vite.config.ts
 npx vite build --config vite.config.internal.ts
 npm run build-types
--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# replace duplicate footer tag
+sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+
 npm run docs:serve
