f06b116fec8579baba4778e454958fcb56093b6c
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-06-02 21:31:05 +0200

    chore: remove unused PageMeta component override

--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -2,14 +2,12 @@ import { defaultTheme } from '@vuepress/theme-default';
 import { path } from '@vuepress/utils';
 
 export default options => {
-    
-    return Object.assign({}, defaultTheme(options),  {
+    return Object.assign({}, defaultTheme(options), {
         name: 'vuepress-theme-local',
         extends: defaultTheme(),
         alias: {
             '@discue/ui-components': path.resolve(__dirname, '../../../src/index'),
-            '@theme/HomeFooter.vue': path.resolve(__dirname, './components/HomeFooter.vue'),
-            '@theme/PageMeta.vue': path.resolve(__dirname, './components/PageMeta.vue'),
+            '@theme/HomeFooter.vue': path.resolve(__dirname, './components/HomeFooter.vue')
         }
     })
 }
