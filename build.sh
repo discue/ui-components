6f15eb82fa717c45dff48a12caf6e4508a3b9063
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-06-20 22:58:48 +0200

    chore(build): dont fail if tsc reports error and type file was created

--- build.sh
+++ build.sh
@@ -5,10 +5,16 @@ set -exu
 rm -rf dist
 
 # replace duplicate footer tag
-sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
-sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/VPPageMeta.vue
+sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/VPPageMeta.vue
 
 npx vite build --config vite.config.ts
 npx vite build --config vite.config.internal.ts
-npm run build-types
+npm run build-types || true
+
+if [[ ! -e dist/ui.es.d.ts ]]; then
+    echo "Types file was not created. Exiting early"
+    exit 1
+fi
+
 node create_theme_keys.js
