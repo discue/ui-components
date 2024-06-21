cfa1371d44d264a3f9a115175eb9f48ee48a4026
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-06-21 23:23:35 +0200

    chore(development): make script fail if commands fail

--- run-dev.sh
+++ run-dev.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -exu
+
 # replace duplicate footer tag
-sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
-sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/VPPageMeta.vue
+sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/VPPageMeta.vue
 
 npm run docs:serve
