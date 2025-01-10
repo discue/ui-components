77c7a1b38be889b20af950c31ab839dd6ca53eca
Stefan Pfaffel <s.pfaffel@gmail.com>
2025-01-10 19:30:11 +0100

    chore(docs): resolve warnings about plugin usage

--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -1,13 +1,3 @@
 import { defaultTheme } from '@vuepress/theme-default';
-import { path } from '@vuepress/utils';
 
-export default options => {
-    return Object.assign({}, defaultTheme(options), {
-        name: 'vuepress-theme-local',
-        extends: defaultTheme(),
-        alias: {
-            '@discue/ui-components': path.resolve(__dirname, '../../../src/index'),
-            '@theme/HomeFooter.vue': path.resolve(__dirname, './components/HomeFooter.vue')
-        }
-    })
-}
+export default defaultTheme
