fba724b8a43c0e55e7da2c71ec9a06d21edcb77e
Stefan Pfaffel <s.pfaffel@gmail.com>
2026-03-29 21:19:36 +0200

    chore(deps): update dependencies
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- prepare-docs-build.sh
+++ prepare-docs-build.sh
@@ -3,5 +3,5 @@
 set -exu
 
 # replace duplicate footer tag
-sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/VPPageMeta.vue
-sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/VPPageMeta.vue
+sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/dist/client/components/VPPageMeta.vue
+sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/dist/client/components/VPPageMeta.vue
